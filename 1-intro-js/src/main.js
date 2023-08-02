40374e218419e89e052c08f616b0109816a7ccce
Gabriel Molteni <fgmolteni@gmail.com>
2023-08-02 12:50:58 -0300

    funciones y variables

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -1,5 +1,9 @@
+function sayHello(name = 'persona', age = 0) {
+  const greeting = `Hola mundo Funcion! ${name} ${age}`;
+  // console.log("Hello world!");
 
-
-funtion sayHello() {
-  console.log("Hello world!");
+  return greeting;
 }
+const result = sayHello('gabi');
+
+console.log(result);
