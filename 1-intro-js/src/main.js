82327ebfecf9ba8c24d519b129c45a91f92217eb
Gabriel Molteni <fgmolteni@gmail.com>
2023-08-03 12:56:26 -0300

    Atributos y tipo de arreglos en Objetos

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -9,19 +9,41 @@ const invoice = {
     lastname: "Dune",
     age: 20,
   },
-  total: 1000,
+  items: [
+    {
+      producto: "Keyboard",
+      price: 399,
+      quantity: 2,
+    },
+    {
+      producto: "Mouse",
+      price: 200,
+      quantity: 1,
+    },
+    {
+      producto: "Paper",
+      price: 100,
+      quantity: 10,
+    },
+  ],
+  total: function () {
+    let total = 0;
+    this.items.forEach(item => {
+      total = total + (item.price * item.quantity);
+    });
+    return total;
+  },
   greeting: function () {
     // no puede ir una funcion de flecha
     return `Hola ${this.client.firstname}`;
   },
 };
 
-
 invoice.client.firstname = "Josep";
-invoice.total = 5000;
+// invoice.total = 5000;
 console.log(invoice);
 
 const greeting = invoice.greeting();
 console.log(greeting);
 
-
+console.log('Total: ' + invoice.total());
