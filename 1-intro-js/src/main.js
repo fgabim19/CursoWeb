0165fe77eb34615a49937b87d8d77e14c125a07e
Gabriel Molteni <fgmolteni@gmail.com>
2023-08-02 23:13:03 -0300

    objetos y

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -1,1 +1,27 @@
+// object
+
+const invoice = {
+  id: 10,
+  date: new Date(),
+  client: {
+    id: 2,
+    firstname: "Helga",
+    lastname: "Dune",
+    age: 20,
+  },
+  total: 1000,
+  greeting: function () {
+    // no puede ir una funcion de flecha
+    return `Hola ${this.client.firstname}`;
+  },
+};
+
+
+invoice.client.firstname = "Josep";
+invoice.total = 5000;
+console.log(invoice);
+
+const greeting = invoice.greeting();
+console.log(greeting);
+
 
